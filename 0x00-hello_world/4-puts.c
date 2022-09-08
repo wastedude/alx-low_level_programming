93c98eb3264fc059151d6e9d58e43a7537b1bb01
wastedude <joramwayne9237@gmail.com>
2022-09-08 02:58:56 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
-#include<stdio.h>
+ #include<stdio.h>
 int main()
 {
-  puts("\"Programming is like building a multilingual puzzle")
-  return(0)
+   puts("\"Programming is like building a multilingual puzzle");
+   return(0);
 }
--Wall gcc
-gcc 4-puts.c
